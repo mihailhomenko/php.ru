d53d59794a14c2735418c426e6da0834481abcbc
misha homenko <mihail.homenko@yandex.ru>
2016-05-13 01:09:48 +0400

    роутер

--- Modules/Router.php
+++ Modules/Router.php
@@ -15,8 +15,8 @@ class Router
 	{   
     if ((isset($module))&&(isset($action)))
     {
-        return call_user_func(__NAMESPACE__ .'\\'.$module.'Module::'.$action.'CommentAction');
-    } else { return call_user_func(__NAMESPACE__.'\\'.App\Config::get('defolt_module').'Module::'.App\Config::get('defolt_action').'CommentAction');
+        return call_user_func_array(__NAMESPACE__ .'\\'.$module.'Module::'.$action.'CommentAction',array());
+    } else { return call_user_func_array(__NAMESPACE__.'\\'.App\Config::get('defolt_module').'Module::'.App\Config::get('defolt_action').'CommentAction',array());
     }
 	}
 }
